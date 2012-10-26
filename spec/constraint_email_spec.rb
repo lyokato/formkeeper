04a2d6b6d42d39ee1ad90f2850a97e241c351c9a
lyokato <lyo.kato@gmail.com>
2012-10-26 14:37:25 +0900

    added files

---
+++ spec/constraint_email_spec.rb
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+require 'spec_helper'
+
+describe FormKeeper::Constraint::Email do
+
+  it "validate value correctly" do
+    constraint = FormKeeper::Constraint::Email.new
+    constraint.validate('lyo.kato@gmail.com', true).should be_true
+    constraint.validate('foo.foo.foo@docomo.ne.jp', true).should be_true
+    constraint.validate('foo.@docomo.ne.jp', true).should be_true
+    constraint.validate('foo..foo@docomo.ne.jp', true).should be_true
+    constraint.validate('lyo.kato', true).should_not be_true
+    constraint.validate('foo@', true).should_not be_true
+    constraint.validate('@foo', true).should_not be_true
+    constraint.validate('.foo@example.com', true).should_not be_true
+    constraint.validate('foo[@example.com', true).should_not be_true
+    constraint.validate('foo @example.com', true).should_not be_true
+  end
+
+end
---
+++ spec/filter_utf8_spec.rb
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+require 'spec_helper'
+
+describe FormKeeper::Filter::ToUTF8 do
+
+  it "handles utf-8 value correctly" do
+
+    value = File.open(File.dirname(__FILE__) + '/asset/utf8.txt') { |f| f.read.chomp }
+    filter = FormKeeper::Filter::ToUTF8.new('UTF-8')
+    filter.process(value).should == "ほげほげ";
+  end
+
+  it "handles euc-jp value correctly" do
+
+    value = File.open(File.dirname(__FILE__) + '/asset/euc.txt') { |f| f.read.chomp }
+    filter = FormKeeper::Filter::ToUTF8.new('EUC-JP')
+    filter.process(value).should == "ほげほげ"
+  end
+
+end
