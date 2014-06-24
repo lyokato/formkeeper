c78ebce963301d23a144db22a7e1ef004b39526e
Darren Coxall <darren.coxall@simplybusiness.co.uk>
2014-06-24 15:28:57 +0100

    Spec was failing...
    Undefined method true?

--- spec/combination_any_spec.rb
+++ spec/combination_any_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 
 describe FormKeeper::CombinationConstraint::Any do
 
-  it "validate value correctly" do
+  it "validates value correctly" do
 
     constraint = FormKeeper::CombinationConstraint::Any.new
-    constraint.validate(['aaaa', 'bbbb'], true).should be_true
-    constraint.validate(['', ''], true).should_not be_true
-    constraint.validate(['', nil], true).should_not be_true
-    constraint.validate(['aaa', nil], true).should be_true
-    constraint.validate(['aaa', 'bbbb', 'cccc', nil], true).should be_true
+    constraint.validate(['aaaa', 'bbbb'], true).should eql(true)
+    constraint.validate(['', ''], true).should_not eql(true)
+    constraint.validate(['', nil], true).should_not eql(true)
+    constraint.validate(['aaa', nil], true).should eql(true)
+    constraint.validate(['aaa', 'bbbb', 'cccc', nil], true).should eql(true)
 
   end
 
